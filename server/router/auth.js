ef409b8fbb4f6f907e9d20379ab3fe5fab6238b2
Wasif Abbas <WasifAbbas900@gmail.com>
2022-10-16 15:10:17 +0530

    first

--- server/router/auth.js
+++ server/router/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 require("../DB/connection");
 const Product = require("../model/productSchema");
 const User = require("../model/userSchema");
+const Order = require("../model/orderSchema");
 const authenticate = require("../middleware/authentication");
 
 // add Product
@@ -37,14 +38,14 @@ router.post("/addProduct", async (req, res) => {
 
 router.post("/registation", async (req, res) => {
   const { name, mobile, email, pass, cpass } = req.body;
-  console.log(req.body);
   if (!name || !email || !mobile || !pass || !cpass) {
     return res.status(422).json({ error: " Check all require feild" });
   }
   try {
     const userExist = await User.findOne({ mobile: mobile });
-    if (userExist) {
+    if (userExist?.mobile) {
       console.log("user already exist");
+      return res.status(422);
     }
     const user = new User({
       name,
@@ -54,7 +55,7 @@ router.post("/registation", async (req, res) => {
       cpass,
     });
     let x = await user.save();
-    console.log(x);
+    // console.log(x);
     res.status(201).json({ message: "registation successfully" });
   } catch (err) {
     console.log(err);
@@ -65,14 +66,13 @@ router.post("/registation", async (req, res) => {
 router.post("/signin", async (req, res) => {
   try {
     const { email, pass } = req.body;
-    const password = req.body;
     if (!email || !pass) {
       return res.status(400).json({ error: "Fill All Feilds" });
     }
     const userLogin = await User.findOne({ email: email });
     if (userLogin) {
       if (pass !== userLogin.pass) {
-        res.status(400).json({ message: "Incalid Password" });
+        res.status(400).json({ message: "Invalid Password" });
       }
       res.json({ message: "user Sign in successfully" });
     } else {
@@ -81,6 +81,83 @@ router.post("/signin", async (req, res) => {
   } catch (err) {
     console.log(err);
   }
+});
+
+//order
+
+router.post("/order", async (req, res) => {
+  const {
+    fname,
+    lname,
+    companyName,
+    street,
+    city,
+    state,
+    pinCode,
+    mobile,
+    email,
+    productName,
+    quantity,
+    price,
+    pMode,
+  } = req.body;
+
+  if (
+    !fname ||
+    !lname ||
+    !street ||
+    !city ||
+    !state ||
+    !pinCode ||
+    !mobile ||
+    !email ||
+    !productName ||
+    !quantity ||
+    !price ||
+    !pMode
+  ) {
+    return res.status(400).json({ error: "Check All Required Feild" });
+  }
+  try {
+    const order = new Order({
+      fname,
+      lname,
+      companyName,
+      street,
+      city,
+      state,
+      pinCode,
+      mobile,
+      email,
+      productName,
+      quantity,
+      price,
+      pMode,
+    });
+    let x = await order.save();
+    // console.log(x);
+    return res.status(201).json({ message: "Order Placed" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//admin
+router.get("/admin", async (req, res) => {
+  let data = await Order.find({});
+  return res.status(200).json({ success: true, message: data });
+});
+
+//user
+router.get("/user", async (req, res) => {
+  let data = await User.find({});
+  return res.status(200).json({ success: true, message: data });
+});
+
+//sofalist
+router.get("/sofa", async (req, res) => {
+  let data = await Product.find({ category: "Sofa" });
+  return res.status(200).json({ success: true, message: data });
 });
 
 module.exports = router;
